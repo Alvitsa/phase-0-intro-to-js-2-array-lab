60b38a1ad802a5e0ab747ce556c338a0ecb9cc69
+ <+>
2024-03-21 12:57:21 +0300

    Creates arrays in js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+function appendCat(name) {
+  let allCats = [...cats,name];
+  return allCats;
+}
+function prependCat(name) {
+  let allCats = [name,...cats];
+  return allCats;
+} 
+function removeLastCat() {
+  let allCats =cats.slice(0,-1);
+  return allCats;
+}
+function removeFirstCat() {
+  let allCats = cats.slice(1);
+  return allCats;
+}
